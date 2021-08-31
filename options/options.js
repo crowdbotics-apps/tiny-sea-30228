0a040d981802a4c04ce79b9c4d6f5263fa7ca313
Crowdbotics <team@crowdbotics.com>
2021-08-31 18:53:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "tiny_sea_30228Identifier",
+  url: "https://tiny_sea_30228Identifier.botics.co",
+  api: "https://tiny_sea_30228Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
